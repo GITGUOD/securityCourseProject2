332ac3c6a11d42eee785255069acc09685c1fea6
Natchapan Tanachokboonyarat <natchapantanachokboonyarat@MacBook-Pro-som-tillhor-Natchapan.local>
2025-03-21 14:02:56 +0100

    The server and client is now communicating with each other

--- TLS/Client.java
+++ TLS/Client.java
@@ -11,9 +11,12 @@ public class Client {
     private static final String PKCS12Location = "/Users/natchapantanachokboonyarat/Desktop/securityCourseProject2/certificates/client.p12"; //'<path>/server.p12 Update the path to PKCS12 file, KLART!;
     private static final String PKCS12Password = "Tonny2002"; // Update if password changed
     //Add custom TrustStore password if not using cacerts
-    //private static final String TSPassword = "changeit";
+    private static final String TRUSTSTORE_LOCATION = "/Users/natchapantanachokboonyarat/Desktop/securityCourseProject2/certificates/client_truststore.jks"; // Update the path
+    private static final String TRUSTSTORE_PASSWORD = "Tonny2002"; //change it är defaultlösenordet på truststore
     public static void main(String[] args) throws Exception {
         char[] passphrase_ks = PKCS12Password.toCharArray();
+        char[] passphrase_ts = TRUSTSTORE_PASSWORD.toCharArray();
+
         //Adding custom TrustStore
         //char[] passphrase_ts = CACERTSPassword.toCharArray();
         //KeyStore ts = KeyStore.getInstance("PKCS12");
@@ -24,23 +27,26 @@ public class Client {
         // Add code for Keystore
         // ?? Allt under är min kod fram tills 'KLART'
         KeyStore ks = KeyStore.getInstance("PKCS12");
-
         try (FileInputStream fis = new FileInputStream(PKCS12Location)) {
             ks.load(fis, passphrase_ks);
         }
         // Initialize a KeyManagerFactory with the KeyStore
         KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
         kmf.init(ks, passphrase_ks);
-
         KeyManager[] keyManagers = kmf.getKeyManagers();
+
+        //Load custom truststore
+        KeyStore ts = KeyStore.getInstance("JKS");
+        try (FileInputStream fil = new FileInputStream(TRUSTSTORE_LOCATION)) {
+            ts.load(fil, passphrase_ts);
+        }
 
         // Obtain the default TrustManagers for the system’s truststore (cacerts)
         TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
-
-        tmf.init((KeyStore) null); // Use the system’s truststore ’cacerts’
-
+        tmf.init((KeyStore) ts); // Use the custom truststore ’cacerts’
         TrustManager[] trustManagers = tmf.getTrustManagers();
         
+
         SSLContext context = SSLContext.getInstance("TLSv1.3");
         //TrustManager (2nd argu) is null to use the default trust manager cacerts
         //To use custom TrustStore, 2nd argument changes to ’trustManagers’
